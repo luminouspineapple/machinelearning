9d1555bc543bd60597a34ac9d131855677be10cb
Alex Selby <alex.selby@pobox.com>
2018-02-18 19:01:49 +0000

    Add display code, timing code and comments

--- test1.py
+++ test1.py
@@ -1,10 +1,12 @@
+from __future__ import print_function
 import numpy as np
 import tensor
+from time import clock
 from mnistread import training_images, training_labels, test_images, test_labels, getpim, disp
 
 kernel=np.array([
-  [[-3,0,3],[-10,0,10],[-3,0,3]],
-  [[-3,-10,-3],[0,0,0],[3,10,3]]
+  [[-3,0,3],[-10,0,10],[-3,0,3]], # Vertical edge detector
+  [[-3,-10,-3],[0,0,0],[3,10,3]], # Horizontal edge detector
 ],dtype=np.float32)/20
 kernel=np.moveaxis(kernel,0,2).reshape((3,3,1,2))
 
@@ -17,4 +19,9 @@ imagenode=tensor.PlaceHolder("images")
 kernelnode=tensor.Constant(kernel)
 edgesnode=tensor.Conv2d(imagenode,kernelnode)
 
+t0=clock()
 edges=edgesnode.evaluate(results={imagenode:training_images})
+print("Time taken to apply convolutions to",len(edges),"images over",kernel.shape[3],"channels =",clock()-t0,"seconds")
+
+disp(floattouint8(edges[2,:,:,0]))
+disp(floattouint8(edges[2,:,:,1]))
